9bb7d0548920fbf88e55c9fa69fc2c3c6d14e757
raphael <raphaelgregg@gmail.com>
2020-03-28 16:51:39 -0300

    Backend da aplicação

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,16 @@
+/**
+ * Responsavel por especificar incidents
+ */
+const connection = require('../database/connection');
+
+module.exports = {
+    async index (request, response) {
+        const ong_id = request.headers.authorization; // acessa dados da sessão logada pelo cabeçalho
+
+        const incidents = await connection('incidents')
+        .where('ong_id', ong_id)
+        .select('*');
+
+        return response.json(incidents);
+    }
+}
---
+++ backend/src/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors'); 
+const routes = require('./routes');
+
+const app = express();
+
+//Versao de produção colocar onde o app esta hospedado
+// app.use(cors({
+//     origin: 'http://meuapp.com'
+// }));
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rota / Recursos
+ */
+
+ /**
+  * Métodos HTTP
+  * 
+  * GET: Buscar/listar uma informação do back-end
+  * POST: Cria uma informação no back-end
+  * PUT: Alterar uma informação no back-end
+  * DELETE: deletar uma  informação no back-end
+  * 
+  */
+
+  /**
+   * Tipos de parâmetros:
+   * 
+   * Query: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+   * Route Params: Parâmetros utilizados para identificar recursos
+   * Request Body: Corpo da requisição, uitilizado para criar ou alterar recursos
+   */
+
+   /**
+    * SQL: MySql, SQLite, PostSQL, Oracle, Microsoft SQL Server
+    * NoSQL: MongoDB, CouchDB, etc
+    */
+
+    /**
+     * Driver: SELECT * from users
+     * Query Builder: table('users').select('*').where()
+     */
+
+
+
